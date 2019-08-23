e559bd04fc34a05cfa76d2260e7c0f5204739077
Frank Noirot <johnson.5208@osu.edu>
2019-08-23 12:01:44 -0400

    Added netlify-cms-widget-color-picker npm package

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,14 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+import * as ColorWidget from 'netlify-cms-widget-color'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+
+CMS.registerWidget('color', ColorWidget.Control)
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
