d3c081c5adaafb0ba76f8d0f4116c98ca10e22fc
Frank Noirot <frankjohnson1993@gmail.com>
2019-10-22 03:32:59 -0400

    Added Carousel module I feel great.

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -5,7 +5,26 @@
         <h1>{{$page.project.title}}</h1>
         <p class="season">{{ $page.project.season }}</p>
       </section>
-      <div class="carousel"></div>
+      <div class="carousel">
+        <div class='stage'></div>
+        <button class="arrow left" v-on:click="() => moveImg(-1)"></button>
+        <button class="arrow right" v-on:click="() => moveImg(1)"></button>
+        <div class="tabs">
+          <button class="step" v-for="(step, j) in processSteps" :key="j"
+            v-on:click="() => activateStep(j)"
+            :class="{active: (currStep === j) ? true: false}" >
+            {{ step }}
+          </button>
+        </div>
+        <div class="dots">
+            <button v-for="(image, i) in images" :key="i"
+              class="dot" :class="{[image.step]: image.step,
+              stepActive: (processSteps[currStep].toLowerCase() === image.step) ? true : false,
+              imgActive: (currImg === i) ? true : false}"
+              :data-to="image.img" v-on:click="() => setImg(i)"></button>
+          </div>
+        </div>
+      </div>
       <section class="body" v-html="$page.project.content"></section>
     </article>
   </Layout>
@@ -44,11 +63,41 @@ export default {
   },
   data: function() {
     return {
-      currCarouselID: 0,
+      currStep: 0,
       currImg: 0
     }
   },
   computed: {
+    processSteps() {
+      return this.$page.project.processSteps.map(step => step.name)
+    },
+    images() {
+      let flatten = function(a, shallow, r){
+        if(!r){ r = []}
+          
+        if (shallow) {
+        return r.concat.apply(r,a);
+        }
+          console.log(a, r)
+          for(var i=0; i<a.length; i++){
+              if(a[i].constructor == Array){
+                  flatten(a[i],shallow,r);
+              }else{
+                  r.push(a[i]);
+              }
+          }
+          return r;
+        }
+
+      return flatten(this.$page.project.processSteps.map(step => 
+        step.images.map(img => {
+          return {
+            img,
+            step: step.name.toLowerCase()
+          }
+        }
+      )), false)
+    },
     primaryRGB: function() {
       return {
         r: this.$page.project.color[0].r,
@@ -61,14 +110,25 @@ export default {
         '--primary-rgb': this.primaryRGB.r+","+this.primaryRGB.g+","+this.primaryRGB.b
       }
     },
-    currentStep: function() {
-      return this.$page.project.processSteps[this.currCarouselID]
-    },
-    currCarouselDOM: function() {
-      return this.$el.querySelector(`#process-btn_${this.currCarouselID}`)
-    }
   },
   methods: {
+    activateStep(step) {
+      this.currStep = step
+    },
+    moveImg(n) {
+      if (this.currImg + n >= this.images.length) {
+        this.currImg = 0
+      } else if (this.currImg + n < 0) {
+        this.currImg = this.images.length - 1
+      } else {
+        this.currImg += n
+      }
+      this.currStep = this.processSteps.map(step => step.toLowerCase()).indexOf(this.images[this.currImg].step)
+    },
+    setImg(n) {
+      this.currImg = n
+      this.currStep = this.processSteps.map(step => step.toLowerCase()).indexOf(this.images[this.currImg].step)
+    }
   }
 }
 </script>
@@ -113,6 +173,110 @@ export default {
     grid-column: 2 / 3;
     align-self: stretch;
     justify-self: stretch;
+    display: grid;
+    grid-template-columns: 5vw 1fr 5vw;
+    grid-template-rows: 1fr 5vh 5vh;
+  }
+  .stage {
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+    align-self: stretch;
+    justify-self: stretch;
+    border-radius: 3vmin;
+    background: linear-gradient(to bottom, var(--bg), white);
+    box-shadow: var(--shadows);
+    z-index: 5;
+  }
+  .arrow {
+    grid-row: 1 / 2;
+    width: 50%;
+    --thick: .5vmin;
+    align-self: center;
+    justify-self: center;
+    border: none;
+    position: relative;
+    transform: scale(1);
+    transition: transform .13s ease-in-out;
+  }
+  .arrow:hover {
+    transform: scale(1.05);
+  }
+  .arrow, .arrow::before, .arrow::after {
+    height: var(--thick);
+    background: rgb(var(--primary-rgb));
+    border-radius: calc(.5 * var(--thick));
+  }
+  .arrow::before, .arrow::after {
+    content: '';
+    position: absolute;
+    width: 40%;
+    top: 50%;
+    left: 0;
+    transform-origin: center left;
+  }
+  .arrow::before {
+    transform: translate(0, -50%) rotate(-45deg) translate(0, calc(.25 * var(--thick)));
+  }
+  .arrow::after {
+    transform: translate(0, -50%) rotate(45deg) translate(0, calc(-.25 * var(--thick)));
+  }
+  .right::after, .right::before {
+    left: initial;
+    right: 0;
+    transform-origin: center right;
+  }
+  .right::before {
+    transform: translate(0, -50%) rotate(-45deg) translate(0, calc(-.25 * var(--thick)));
+  }
+  .right::after {
+    transform: translate(0, -50%) rotate(45deg) translate(0, calc(.25 * var(--thick)));
+  }
+  .tabs {
+    grid-row: 2 / 3;
+    grid-column: 2 / 3;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .step {
+    color: rgb(var(--primary-rgb));
+    justify-self: stretch;
+    border: none;
+    background: transparent;
+    border-radius: 0 0 2vmin 2vmin;
+    width: 100%;
+    justify-self: stretch;
+    align-self: stretch;
+    padding-top: 10vh;
+    margin-top: -10vh;
+  }
+  .active {
+    background: rgba(var(--primary-rgb), .6);
+    color: var(--bg);
+  }
+  .dots {
+    grid-row: 3 / 4;
+    grid-column: 2 / 3;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-self: center;
+  }
+  .dot {
+    width: .75vmin;
+    height: .75vmin;
+    padding: 0;
+    background: transparent;
+    border-radius: 50%;
+    margin: 1vmin auto;
+    border: solid 1px rgb(var(--primary-rgb));
+    transform: scale(1);
+    transition: transform .13s ease-in-out;
+  }
+  .stepActive {
+    transform: scale(2);
+  }
+  .imgActive {
     background: rgb(var(--primary-rgb));
   }
 </style>
